5a1285837c79c5ec58edc26f795c670c3a3d9c27
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2023-05-20 16:50:16 -0300

    create user github auth login

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
+import { z } from 'zod'
 export async function memoriesRoutes(app: FastifyInstance) {
   app.get('/memories', async () => {
     const memories = await prisma.memory.findMany({
@@ -7,12 +8,77 @@ export async function memoriesRoutes(app: FastifyInstance) {
         createdAt: 'asc',
       },
     })
-    return memories
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat('...'),
+      }
+    })
   })
 
-  app.get('/memories/:id', async () => {})
+  app.get('/memories/:id', async (request) => {
+    const paramsScheme = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = paramsScheme.parse(request.params)
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+    return memory
+  })
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
 
-  app.post('/memories', async () => {})
-  app.put('/memories', async () => {})
-  app.delete('/memories', async () => {})
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: '8c9a92fa-7025-42c8-ac1b-a15b871d3f72',
+      },
+    })
+    return memory
+  })
+  app.put('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = paramsSchema.parse(request.params)
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.params)
+    const memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
+    return memory
+  })
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = paramsSchema.parse(request.params)
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    })
+  })
 }
---
+++ web/src/components/EmptyMemories.tsx
@@ -0,0 +1,12 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <p className="text-center leading-relaxed w-[360px]">
+        Você ainda não registrou nemhuma lembrança,comece a{" "}
+        <a href="" className="underline hover:text-gray-50">
+          criar agora!
+        </a>
+      </p>
+    </div>
+  );
+}
